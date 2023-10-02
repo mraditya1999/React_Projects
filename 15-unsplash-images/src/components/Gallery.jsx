127a243119248f9504d0406d187aac43e887350d
Aditya Yadav <ay5480620@gmail.com>
2023-10-02 12:03:34 +0530

    initial commit

---
+++ 15-unsplash-images/src/components/Gallery.jsx
@@ -0,0 +1,56 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { useGlobalContext } from '../context';
+
+const url = `https://api.unsplash.com/search/photos?client_id=${
+  import.meta.env.VITE_API_KEY
+}`;
+
+const Gallery = () => {
+  const { searchTerm } = useGlobalContext();
+  const response = useQuery({
+    queryKey: ['images', searchTerm],
+    queryFn: async () => {
+      const result = await axios.get(`${url}&query=${searchTerm}`);
+      return result.data;
+    },
+  });
+
+  const { isLoading, isError, data } = response;
+
+  if (isLoading) {
+    return (
+      <section className='image-container'>
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+
+  if (isError) {
+    return (
+      <section className='image-container'>
+        <h4>There was an Error...</h4>
+      </section>
+    );
+  }
+
+  const results = data.results;
+  if (results.length < 1) {
+    return (
+      <section className='image-container'>
+        <h4>No results found...</h4>
+      </section>
+    );
+  }
+
+  return (
+    <section className='image-container'>
+      {results.map((item) => {
+        const { id, alt_description } = item;
+        const url = item?.urls?.regular;
+        return <img src={url} alt={alt_description} key={id} className='img' />;
+      })}
+    </section>
+  );
+};
+export default Gallery;
---
+++ 15-unsplash-images/src/components/ThemeToggle.jsx
@@ -0,0 +1,19 @@
+import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
+import { useGlobalContext } from '../context';
+
+const ThemeToggle = () => {
+  const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
+
+  return (
+    <section className='toggle-container'>
+      <button className='dark-toggle' type='button' onClick={toggleDarkTheme}>
+        {isDarkTheme ? (
+          <BsFillMoonFill className='toggle-icon' />
+        ) : (
+          <BsFillSunFill className='toggle-icon' />
+        )}
+      </button>
+    </section>
+  );
+};
+export default ThemeToggle;
